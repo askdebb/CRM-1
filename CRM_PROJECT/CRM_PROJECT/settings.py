fbc243f11060185e3027d9c38295f0c69de46ef5
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-02-02 16:17:03 +0000

    deployment configurations

--- CRM_PROJECT/CRM_PROJECT/settings.py
+++ CRM_PROJECT/CRM_PROJECT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)76fi37i_1l&_w6&1z%bkb#midz61rx@98-#wn+*m@4q%xx!$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -122,12 +122,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'), 
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- CRM_PROJECT/CRM_PROJECT/urls.py
+++ CRM_PROJECT/CRM_PROJECT/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gbc_crm.urls')),
     path("select2/", include("django_select2.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
